3734ae816871eec1b23fa9cc36656b7708521a97
sachinmagadum <sachinmagadum5009@gmail.com>
2024-02-18 10:52:16 +0530

    Added code for multiplication

--- arithmatic.py
+++ arithmatic.py
@@ -7,3 +7,14 @@ number1 = 10
 number2 = 20
 ans = addition(number1, number2)
 print(f"Addition of {number1} and {number2} is {ans}")
+
+
+def multiplication(no1, no2):
+    ans = no1 * no2
+    return ans
+
+
+number1 = 10
+number2 = 20
+ans = multiplication(number1, number2)
+print(f"Multiplication of {number1} and {number2} is {ans}")
