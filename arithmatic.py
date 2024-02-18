57a1f0a3608bef5e20db629ddbb28e22dab5d8ef
sachinmagadum <sachinmagadum5009@gmail.com>
2024-02-18 11:18:16 +0530

    Added code for Division

--- arithmatic.py
+++ arithmatic.py
@@ -18,3 +18,13 @@ number1 = 10
 number2 = 20
 ans = multiplication(number1, number2)
 print(f"Multiplication of {number1} and {number2} is {ans}")
+
+def Division(no1, no2):
+    ans = no1 / no2
+    return ans
+
+
+number1 = 20
+number2 = 10
+ans = Division(number1, number2)
+print(f"Division of {number1} and {number2} is {ans}")
